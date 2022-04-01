0d316c24a4a05a8b32ebe6f3e198d6a032e7d66c
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2022-04-01 11:48:07 +0200

    feat: add Router component in App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./containers/Footer";
 import Header from "./containers/Header";
+import Router from "./router/Router";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header />
+        <Router />
         <Footer />
       </BrowserRouter>
     </>
