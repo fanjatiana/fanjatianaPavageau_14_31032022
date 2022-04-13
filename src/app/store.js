26de6edcb946ff90eb2f28ba1468ea1b71281e3f
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2022-04-13 22:19:08 +0200

    feat : added redux persist for saving data on page reload

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
-import newEmployeeReducer from '../features/employee/employeeSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import newEmployeeReducer from "../features/employee/employeeSlice";
 
-export const store = configureStore({
-  reducer: {
-    employees : newEmployeeReducer,
-  },
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const reducers = combineReducers({
+  employees: newEmployeeReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  //devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
+});
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
+import React from "react";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
 
-import {createRoot} from 'react-dom/client';
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement)
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+let persistor = persistStore(store);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
-  </React.StrictMode>,
-
+  </React.StrictMode>
 );
-
